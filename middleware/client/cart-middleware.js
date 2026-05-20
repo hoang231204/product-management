95d6e0a3a867fa2dc11f3255e8f0755a4dfed40a
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-21 00:11:59 +0700

    refactor(cart): merge guest cart to user cart and clean up empty cart records

--- middleware/client/cart-middleware.js
+++ middleware/client/cart-middleware.js
@@ -4,9 +4,27 @@ module.exports.cart = async (req, res, next) => {
   try {
     let cart = null;
     if (res.locals.user) {
-    
       cart = await Cart.findOne({ user_id: res.locals.user._id });
       if (cart) {
+        if (req.cookies.cartId && req.cookies.cartId !== cart._id.toString()) {
+          const guestCart = await Cart.findById(req.cookies.cartId);
+          if (guestCart) {
+            if (guestCart.products && guestCart.products.length > 0) {
+              guestCart.products.forEach(guestItem => {
+                const existingItem = cart.products.find(
+                  item => item.product_id.toString() === guestItem.product_id.toString()
+                );
+                if (existingItem) {
+                  existingItem.quantity += guestItem.quantity;
+                } else {
+                  cart.products.push(guestItem);
+                }
+              });
+              await cart.save();
+            }
+            await Cart.findByIdAndDelete(req.cookies.cartId);
+          }
+        }
         res.cookie('cartId', cart._id.toString(), { maxAge: 1000 * 60 * 60 * 24 * 7 });
         req.cartId = cart._id.toString();
       } 
@@ -34,9 +52,9 @@ module.exports.cart = async (req, res, next) => {
         await cart.save();
         res.cookie('cartId', cart._id.toString(), { maxAge: 1000 * 60 * 60 * 24 * 7 });
       }
-      
       req.cartId = cart._id.toString();
     }
+    
     res.locals.miniCartCount = cart 
       ? cart.products.reduce((sum, item) => sum + item.quantity, 0) 
       : 0;
