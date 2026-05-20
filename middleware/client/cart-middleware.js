f1554b24f89d9265cf9af8616cfd861f2a5d5df9
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-20 10:28:11 +0700

    refactor(cart): optimize cart middleware logic for authenticated and guest users

--- middleware/client/cart-middleware.js
+++ middleware/client/cart-middleware.js
@@ -1,17 +1,49 @@
 const Cart = require('../../models/cart-model');
-module.exports.cart = async (req, res, next) =>{
-    if(!req.cookies.cartId){
-        const cart = new Cart();
-        await cart.save();
-        res.cookie('cartId', cart._id.toString(), {maxAge: 1000 * 60 * 60 * 24 * 7});
+
+module.exports.cart = async (req, res, next) => {
+  try {
+    let cart = null;
+    if (res.locals.user) {
+    
+      cart = await Cart.findOne({ user_id: res.locals.user._id });
+      if (cart) {
+        res.cookie('cartId', cart._id.toString(), { maxAge: 1000 * 60 * 60 * 24 * 7 });
         req.cartId = cart._id.toString();
+      } 
+      else {
+        if (req.cookies.cartId) {
+          cart = await Cart.findById(req.cookies.cartId);
+        }
+        if (cart) {
+          cart.user_id = res.locals.user._id;
+          await cart.save();
+        } else {
+          cart = new Cart({ user_id: res.locals.user._id });
+          await cart.save();
+        }
+        res.cookie('cartId', cart._id.toString(), { maxAge: 1000 * 60 * 60 * 24 * 7 });
+        req.cartId = cart._id.toString();
+      }
+    } 
+    else {
+      if (req.cookies.cartId) {
+        cart = await Cart.findById(req.cookies.cartId);
+      }
+      if (!cart) {
+        cart = new Cart();
+        await cart.save();
+        res.cookie('cartId', cart._id.toString(), { maxAge: 1000 * 60 * 60 * 24 * 7 });
+      }
+      
+      req.cartId = cart._id.toString();
     }
-    else{
-        req.cartId = req.cookies.cartId;
-    }
-    const cart = await Cart.findById({_id: req.cartId});
     res.locals.miniCartCount = cart 
       ? cart.products.reduce((sum, item) => sum + item.quantity, 0) 
       : 0;
+
     next();
-}
+  } catch (error) {
+    console.error("Lỗi xử lý middleware giỏ hàng:", error);
+    res.status(500).send("Lỗi hệ thống");
+  }
+};
