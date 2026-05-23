c96bd5adf874848f7779f0c97c65b190bc6aa026
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-23 23:48:40 +0700

    feat(user management):add function to display user details page

--- controllers/admin/user-controller.js
+++ controllers/admin/user-controller.js
@@ -6,3 +6,27 @@ module.exports.index = async(req, res) =>{
         users: users
     })
 }
+module.exports.details = async(req, res) =>{
+    const id = req.params.id;
+    const user = await User.findOne({_id: id, deleted: false});
+    if(!user){
+        req.flash('error', 'Không tìm thấy người dùng');
+        return res.redirect('/admin/users');
+    }
+    res.render('admin/pages/user/details',{
+        pageTitle: "Chi tiết người dùng",
+        user: user
+    })
+}
+module.exports.delete = async(req, res) =>{
+    const id = req.params.id;
+    const user = await User.findOne({_id: id, deleted: false});
+    if(!user){
+        req.flash('error', 'Không tìm thấy người dùng');
+        return res.redirect('/admin/users');
+    }
+    user.deleted = true;
+    await user.save();
+    req.flash('success', 'Xóa người dùng thành công');
+    return res.redirect('/admin/users');
+}
--- routes/admin/user-route.js
+++ routes/admin/user-route.js
@@ -2,4 +2,5 @@ const userController = require('../../controllers/admin/user-controller');
 const express = require('express');
 const router = express.Router();
 router.get('/', userController.index);
+router.get('/details/:id', userController.details);
 module.exports = router;
