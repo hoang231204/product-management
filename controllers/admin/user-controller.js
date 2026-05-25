f9f1e99f364d06d467b9d486b198ea126c19dd5c
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-25 14:06:32 +0700

    feat(user management):add function to detlete and edit user

--- controllers/admin/user-controller.js
+++ controllers/admin/user-controller.js
@@ -1,4 +1,5 @@
 const User = require('../../models/user-model');
+const systemConfig = require('../../config/system');
 module.exports.index = async(req, res) =>{
     const users = await User.find({deleted: false});
     res.render('admin/pages/user/index',{
@@ -29,5 +30,35 @@ module.exports.changeStatus = async(req, res) =>{
     user.status = status;
     await user.save();
     req.flash('success', 'Cập nhật trạng thái thành công');
-    res.redirect('/admin/users');
+    res.redirect(`${systemConfig.prefixAdmin}/users`);
+}
+module.exports.delete = async(req, res) =>{
+    const id = req.params.id;
+    const user = await User.findOne({_id: id, deleted: false});
+    if(!user){
+        req.flash('error', 'Không tìm thấy người dùng');
+        return res.redirect(`${systemConfig.prefixAdmin}/users`);
+    }
+    user.deleted = true;
+    await user.save();
+    req.flash('success', 'Xóa người dùng thành công');
+    res.redirect(`${systemConfig.prefixAdmin}/users`);
+}
+module.exports.edit = async(req, res) =>{
+    const id = req.params.id;
+    const user = await User.findOne({_id: id, deleted: false, status: "active"});
+    if(!user){
+        req.flash('error', 'Không tìm thấy người dùng');
+        return res.redirect(`${systemConfig.prefixAdmin}/users`);
+    }
+    res.render('admin/pages/user/edit',{
+        pageTitle: "Chỉnh sửa người dùng",
+        user: user
+    })
+}
+module.exports.editPatch = async (req, res) =>{
+    const id = req.params.id;
+    await User.updateOne({_id: id}, req.body);
+    req.flash('success', 'Cập nhật người dùng thành công');
+    res.redirect(`${systemConfig.prefixAdmin}/users`);
 }
--- routes/admin/user-route.js
+++ routes/admin/user-route.js
@@ -1,7 +1,16 @@
 const userController = require('../../controllers/admin/user-controller');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer();
 router.get('/', userController.index);
 router.get('/details/:id', userController.details);
 router.patch('/change-status/:status/:id', userController.changeStatus);
+router.patch('/delete/:id',userController.delete);
+router.get('/edit/:id', userController.edit);
+router.patch(
+    '/edit/:id',
+    upload.single('avatar'),
+    userController.editPatch
+);
 module.exports = router;
