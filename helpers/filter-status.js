c7a1992f41edcdcd4f498e6274e9b413c6b73d80
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-29 13:48:50 +0700

    refactor(helpers): update filterStatus to support multiple types

--- helpers/filter-status.js
+++ helpers/filter-status.js
@@ -1,31 +1,21 @@
-module.exports = (query)=>{
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class:""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class:""
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class:""
-        },
-        {
-            name: "Chờ duyệt",
-            status: "pending",
-            class:""
-        },
-        {
-            name: "Sắp hết",
-            status: "low_stock",
-            class:""
-        }
-    ]
+module.exports = (query, type) => {
+    let allStatuses = [
+        { name: "Tất cả", status: "", class: "" },
+        { name: "Hoạt động", status: "active", class: "" },
+        { name: "Dừng hoạt động", status: "inactive", class: "" },
+        { name: "Sắp hết", status: "low_stock", class: "" },
+        { name: "Chờ duyệt", status: "pending", class: "" },
+        { name: "Đã xác nhận", status: "confirmed", class: "" },
+        { name: "Đang giao hàng", status: "shipping", class: "" },
+        { name: "Đã giao hàng", status: "delivered", class: "" },
+        { name: "Đã hủy", status: "canceled", class: "" }
+    ];
+    const productStatuses = ["", "active", "inactive", "low_stock"];
+    const orderStatuses = ["", "pending", "confirmed", "shipping", "delivered", "canceled"];
+    let filterStatus = (type === 'product') 
+        ? allStatuses.filter(item => productStatuses.includes(item.status))
+        : allStatuses.filter(item => orderStatuses.includes(item.status))
+
     if(query.status){
         const index = filterStatus.findIndex(item => item.status == query.status)
         filterStatus[index].class = "active"
@@ -33,5 +23,5 @@ module.exports = (query)=>{
         const index = filterStatus.findIndex(item => item.status === "")
         filterStatus[index].class = "active"
     }
-    return filterStatus
+    return filterStatus;
 }
