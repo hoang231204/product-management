5b0029c9f65d245125f3816390b9413f161f1e38
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-31 22:28:57 +0700

    refactor(product): add try-catch

--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -1,6 +1,5 @@
 const Product = require('../../models/product-model')
 const ProductCategory = require('../../models/product-category-model')
-const Account = require('../../models/account-model')
 const filter = require("../../helpers/filter-status")
 const search = require("../../helpers/search")  
 const pagination = require("../../helpers/pagination") 
@@ -51,7 +50,9 @@ module.exports.index = async (req, res) => {
             objectPagination: objectPagination
     });
     } catch (error) {
-        res.status(500).send("Lỗi server");
+        console.error("Lỗi khi truy vấn sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi tải sản phẩm.");
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
     } 
 }
 //CẬP NHẬT TRẠNG THÁI
@@ -73,63 +74,72 @@ module.exports.changeStatus =async (req,res)=>{
         req.flash('success', 'Cập nhật trạng thái thành công!')
         res.redirect(backUrl);
     } catch (error) {
-        res.status(500).send("Lỗi server");
+        console.error("Lỗi khi cập nhật trạng thái sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi cập nhật trạng thái sản phẩm.");
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
     }
 }
 //CẬP NHẬT NHIỀU TRẠNG THÁI
 module.exports.changeMulti = async (req, res) => {
-    const idsChecked = req.body.ids; 
-    const typeChecked = req.body.type;
-    const ids = idsChecked.split(","); 
-    let updatedBy = {
-        account_id: res.locals.user._id,
-        updatedAt: new Date()
-    };
-    const permissions = res.locals.role.permissions;
-    switch (typeChecked) {
-        case "delete":
-            if(!permissions.includes("product_delete")){
-                req.flash("error","Bạn không có quyền thực hiện chức năng này!")
-                return res.redirect(`${systemConfig.prefixAdmin}/products`)
-            }
-            await Product.updateMany(
-            { 
-                _id: { $in: ids },
-                deleted: false 
-            },
-            {
-                $set: {
-                    deleted: true,
-                    deletedBy: {
-                        account_id: res.locals.user._id,
-                        deletedAt: new Date()
+    try{
+        const idsChecked = req.body.ids; 
+        const typeChecked = req.body.type;
+        const ids = idsChecked.split(","); 
+        let updatedBy = {
+            account_id: res.locals.user._id,
+            updatedAt: new Date()
+        };
+        const permissions = res.locals.role.permissions;
+        switch (typeChecked) {
+            case "delete":
+                if(!permissions.includes("product_delete")){
+                    req.flash("error","Bạn không có quyền thực hiện chức năng này!")
+                    return res.redirect(`${systemConfig.prefixAdmin}/products`)
+                }
+                await Product.updateMany(
+                { 
+                    _id: { $in: ids },
+                    deleted: false 
+                },
+                {
+                    $set: {
+                        deleted: true,
+                        deletedBy: {
+                            account_id: res.locals.user._id,
+                            deletedAt: new Date()
+                        }
                     }
                 }
-            }
-        );
-            break;
+            );
+                break;
 
-        case "change-position":
-            if(!permissions.includes("product_edit")) {
-                req.flash("error","Bạn không có quyền thực hiện chức năng này!")
-                return res.redirect(`${systemConfig.prefixAdmin}/products`)
-            }
-            for (const item of ids) {
-            const [id, position] = item.split("-");
-            const newPosition = parseInt(position);
-            await Product.updateOne({ _id: id }, { position: newPosition, $push: { updatedBy: updatedBy } });
-            }
-            break;
-        default:
-            if(!permissions.includes("product_edit")) {
-                req.flash("error","Bạn không có quyền thực hiện chức năng này!")
-                return res.redirect(`${systemConfig.prefixAdmin}/products`)
-            }
-            await Product.updateMany({ _id: { $in: ids } }, { status: typeChecked, $push: { updatedBy: updatedBy } }); 
+            case "change-position":
+                if(!permissions.includes("product_edit")) {
+                    req.flash("error","Bạn không có quyền thực hiện chức năng này!")
+                    return res.redirect(`${systemConfig.prefixAdmin}/products`)
+                }
+                for (const item of ids) {
+                    const [id, position] = item.split("-");
+                    const newPosition = parseInt(position);
+                    await Product.updateOne({ _id: id }, { position: newPosition, $push: { updatedBy: updatedBy } });
+                }
+                break;
+            default:
+                if(!permissions.includes("product_edit")) {
+                    req.flash("error","Bạn không có quyền thực hiện chức năng này!")
+                    return res.redirect(`${systemConfig.prefixAdmin}/products`)
+                }
+                await Product.updateMany({ _id: { $in: ids } }, { status: typeChecked, $push: { updatedBy: updatedBy } }); 
+        }
+        req.flash('success', 'Cập nhật trạng thái thành công!');
+        const backUrl = req.get("Referrer");
+        res.redirect(backUrl);
     }
-    req.flash('success', 'Cập nhật trạng thái thành công!');
-    const backUrl = req.get("Referrer");
-    res.redirect(backUrl);
+    catch(error){
+        console.error("Lỗi khi cập nhật trạng thái sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi cập nhật trạng thái sản phẩm.");
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
+    }
 }
 //XÓA MỀM
 module.exports.delete = async (req, res) => {
@@ -158,68 +168,104 @@ module.exports.delete = async (req, res) => {
         );
         req.flash('success', 'Xóa sản phẩm thành công!');
     } catch (error) {
-        console.error("Lỗi xóa sản phẩm:", error);
-        req.flash('error', 'Đã có lỗi xảy ra, không thể xóa sản phẩm.');
+        console.error("Lỗi khi xóa sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi xóa sản phẩm.");
     }
-    const backUrl = req.get("Referrer") || `${systemConfig.prefixAdmin}/products`;
-    res.redirect(backUrl);
+   res.redirect(`${systemConfig.prefixAdmin}/products`);
 };
 //THÙNG RÁC
 module.exports.recycleBin = async (req, res)=>{
-    let find={};
-    find.deleted = true;
-    const products = await Product.find(find).populate("deletedBy.account_id","fullname").lean();
-    res.render("admin/pages/products/recycle-bin",{
-        pageTitle:"Thùng rác",
-        products: products
-    })
+    try {
+        let find={};
+        find.deleted = true;
+        const products = await Product.find(find).populate("deletedBy.account_id","fullname").lean();
+        res.render("admin/pages/products/recycle-bin",{
+            pageTitle:"Thùng rác",
+            products: products
+        })
+    }
+    catch(error){
+        console.error("Lỗi khi truy vấn sản phẩm trong thùng rác:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi tải sản phẩm trong thùng rác.");
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
+    }
 }
 //XÓA VĨNH VIỄN
 module.exports.hardDelete = async (req,res)=>{
-    const dataId = req.params.id;
-    await Product.deleteOne({_id:dataId})
-    req.flash('success', 'Xóa sản phẩm vĩnh viễn thành công!');
-    const backUrl = req.get("Referrer");
-    res.redirect(backUrl);
+    try {
+        const dataId = req.params.id;
+        await Product.deleteOne({_id:dataId})
+        req.flash('success', 'Xóa sản phẩm vĩnh viễn thành công!');
+        const backUrl = req.get("Referrer");
+        res.redirect(backUrl);
+    }
+    catch(error){
+        console.error("Lỗi khi xóa sản phẩm vĩnh viễn:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi xóa sản phẩm vĩnh viễn.");
+        res.redirect(`${systemConfig.prefixAdmin}/products/recycle-bin`);
+    }
 }
 //KHÔI PHỤC
 module.exports.restore = async (req,res)=>{
-    const dataId = req.params.id;
-    await Product.updateOne({_id:dataId},{deleted:false, deletedBy: null})
-    req.flash('success', 'Khôi phục sản phẩm thành công!');
-    const backUrl = req.get("Referrer");
-    res.redirect(backUrl);
+    try {
+        const dataId = req.params.id;
+        let updatedBy = {
+            account_id: res.locals.user._id,
+            updatedAt: new Date()
+        };
+        await Product.updateOne({_id:dataId},{deleted:false, $push: {updatedBy: updatedBy}})
+        req.flash('success', 'Khôi phục sản phẩm thành công!');
+        res.redirect(`${systemConfig.prefixAdmin}/products/recycle-bin`);
+    }
+    catch(error){
+        console.error("Lỗi khi khôi phục sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi khôi phục sản phẩm.");
+        res.redirect(`${systemConfig.prefixAdmin}/products/recycle-bin`);
+    }
 }
 //GET CREATE
 module.exports.create = async (req, res)=>{
-    const categories = await ProductCategory.find({status: "active", deleted: false});
-    const categoryTree = tree(categories);
-    res.render("admin/pages/products/create", {
-        pageTitle: "Tạo mới sản phẩm",
-        categoryTree: categoryTree
-    });
+    try{
+        const categories = await ProductCategory.find({status: "active", deleted: false});
+        const categoryTree = tree(categories);
+        res.render("admin/pages/products/create", {
+            pageTitle: "Tạo mới sản phẩm",
+            categoryTree: categoryTree
+        });
+    } catch (error) {
+        console.error("Lỗi khi tải trang tạo sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi tải trang tạo sản phẩm.");
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
+    }
 }
 //POST CREATE
 module.exports.createPost = async (req, res)=>{
-    const permissions = res.locals.role.permissions;
-    if(!permissions.includes("product_create")){
-        req.flash("error","Bạn không có quyền thực hiện chức năng này!")
-        return res.redirect(`${systemConfig.prefixAdmin}/products`)
+    try{
+        const permissions = res.locals.role.permissions;
+        if(!permissions.includes("product_create")){
+            req.flash("error","Bạn không có quyền thực hiện chức năng này!")
+            return res.redirect(`${systemConfig.prefixAdmin}/products`)
+        }
+        req.body.price = parseInt(req.body.price)
+        req.body.discountPercentage = parseInt(req.body.discountPercentage)
+        req.body.stock = parseInt(req.body.stock)
+        const count = await Product.countDocuments()
+        if(req.body.position == ''){
+            req.body.position = count + 1;
+        }
+        req.body.createdBy = {
+            account_id: res.locals.user._id
+        }
+        const product = new Product(req.body)
+        await product.save();
+        req.flash('success', 'Tạo sản phẩm thành công!');
+        res.redirect(`${systemConfig.prefixAdmin}/products`)
     }
-    req.body.price = parseInt(req.body.price)
-    req.body.discountPercentage = parseInt(req.body.discountPercentage)
-    req.body.stock = parseInt(req.body.stock)
-    const count = await Product.countDocuments()
-    if(req.body.position == ''){
-        req.body.position = count + 1;
+    catch(error){
+        console.error("Lỗi khi tạo sản phẩm:", error);
+        req.flash("error", "Đã có lỗi xảy ra khi tạo sản phẩm.");
+        res.redirect(`${systemConfig.prefixAdmin}/products/create`);
     }
-    req.body.createdBy = {
-        account_id: res.locals.user._id
-    }
-    const product = new Product(req.body)
-    product.save();
-    req.flash('success', 'Tạo sản phẩm thành công!');
-    res.redirect(`${systemConfig.prefixAdmin}/products`)
         
  }
 //GET EDIT
@@ -227,12 +273,9 @@ module.exports.edit= async (req,res)=>{
     try {
         const categories = await ProductCategory.find({deleted: false},{status: "active"});
         const categoryTree = tree(categories);
-        const product = await Product.findById(req.params.id).populate("category");
-        const categoryId = product.category ? product.category._id : null;
-        const categoryTitle = product.category ? product.category.title : "N/A";
-        const updatedBy= {
-
-        }
+        const product = await Product.findById(req.params.id).populate("category_id");
+        const categoryId = product.category_id ? product.category_id._id : null;
+        const categoryTitle = product.category_id ? product.category_id.title : "N/A";
         res.render("admin/pages/products/edit",{
             pageTitle: "Sửa sản phẩm",
             product: product,
@@ -241,7 +284,8 @@ module.exports.edit= async (req,res)=>{
             categoryTitle: categoryTitle
         })
     } catch (error) {
-        res.status(404).send("Sản phẩm không tồn tại");
+        req.flash("error", "Sản phẩm không tồn tại");
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
     }
 }
 //PATCH EDIT
@@ -267,8 +311,7 @@ module.exports.editPatch = async (req,res)=>{
         res.redirect(`${systemConfig.prefixAdmin}/products`);
     } catch (error) {
         req.flash("error","Cập nhật sản phẩm thất bại!");
-        const backUrl = req.get("Referrer");
-        res.redirect(backUrl);
+        res.redirect(`${systemConfig.prefixAdmin}/products/edit/${id}`);
     }
     
 }
@@ -288,7 +331,7 @@ module.exports.details = async (req, res) => {
     if (!product) {
         return res.status(404).send("Sản phẩm không tồn tại");
     }
-    const categoryTitle = product.category?.title || "N/A";
+    const categoryTitle = product.category_id?.title || "N/A";
     res.render("admin/pages/products/details", {
         pageTitle: product.title,
         product: product,
