e0b20dcc9265dac6350ca0379f8e94c1dbcc8984
hoang231204 <nguyenhoang231204@gmail.com>
2026-05-31 21:24:21 +0700

    feat(blog): implement data retrieval and display logic for details viem

--- controllers/admin/post-controller.js
+++ controllers/admin/post-controller.js
@@ -3,6 +3,7 @@ const filter = require('../../helpers/filter-status');
 const search = require('../../helpers/search');
 const pagination = require('../../helpers/pagination');
 const systemConfig = require('../../config/system');
+//GET /posts
 module.exports.index = async (req, res) => {
     const permissions = res.locals.role.permissions;
     if(!permissions.includes("blog_view")){
@@ -38,7 +39,7 @@ module.exports.index = async (req, res) => {
             .populate("createdBy.account_id","fullname")
             .populate("updatedBy.account_id","fullname")
             .lean()
-        res.render('admin/pages/posts/index', {
+        res.render('admin/pages/post/index', {
             pageTitle: "Danh sách bài viết",
             posts: posts,
             filterStatus: filterStatus,
@@ -50,4 +51,35 @@ module.exports.index = async (req, res) => {
         req.flash("error","Có lỗi xảy ra trong quá trình xử lý!")
         res.redirect(`${systemConfig.prefixAdmin}/posts`)
     } 
+}
+//GET /posts/details/:id
+module.exports.details = async (req, res) =>{
+    try{
+        const permissions = res.locals.role.permissions;
+            if(!permissions.includes("blog_view")){
+                req.flash("error","Bạn không có quyền thực hiện chức năng này!")
+                return res.redirect(`${systemConfig.prefixAdmin}/posts`)
+            }
+            const post = await Post
+                .findOne({ _id: req.params.id }) 
+                .populate("category_id", "title")
+                .populate("createdBy.account_id", "fullname")
+                .populate("updatedBy.account_id", "fullname")
+                .lean();
+            if (!post) {
+                req.flash("error", "Bài viết không tồn tại!");
+                return res.redirect(`${systemConfig.prefixAdmin}/posts`);
+            }
+            const categoryTitle = post.category_id ? post.category_id.title : "Không có danh mục";
+            res.render("admin/pages/post/details", {
+                pageTitle: post.title,
+                post: post,
+                categoryTitle: categoryTitle
+            });
+    }
+    catch(error){
+        console.log(error);
+        req.flash("error","Có lỗi xảy ra trong quá trình xử lý!")
+        res.redirect(`${systemConfig.prefixAdmin}/posts`)
+    }
 }
--- routes/admin/post-route.js
+++ routes/admin/post-route.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../../controllers/admin/post-controller');
 router.get("/",postController.index)
+router.get("/details/:id",postController.details)
 module.exports = router;
