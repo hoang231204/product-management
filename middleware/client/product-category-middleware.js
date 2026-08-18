f9330f8d79001cfd8877d1e56cad3990ab6d79ac
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-18 22:27:10 +0700

    refactor: update query

--- middleware/client/product-category-middleware.js
+++ middleware/client/product-category-middleware.js
@@ -1,7 +1,7 @@
 const Category = require('../../models/product-category-model');
 const tree = require('../../helpers/create-tree');
 module.exports.category = async (req, res, next) =>{
-    const categories = await Category.find({deleted: false}).select('title slug parent_id').lean();
+    const categories = await Category.find({deleted: false}).select('title slug parent_id status').lean();
     if(!categories) {
         return next();
     }
