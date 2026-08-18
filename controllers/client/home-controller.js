bd0c816806030fb037ff4aa054c854921d3da459
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-18 22:12:42 +0700

    fix(home): update query to get featured and lastest products

--- controllers/client/home-controller.js
+++ controllers/client/home-controller.js
@@ -3,16 +3,16 @@ const calcuNewPrice = require("../../helpers/calcu-new-price");
 module.exports.index = async (req, res) => {
   try{
     let find={
-    deleted: false,
-    status: "active",
-    featured:"1",
-    stock: {$gt: 0}
+      deleted: false,
+      status: "active",
+      stock: { $gt: 0 },
+      featured: "0"
     }
-    const productsFeatured = await Product.find(find).limit(6);
+    const productsFeatured = await Product.find({featured: "1"}).limit(4);
     productsFeatured.forEach(item => {
       item.priceNew = calcuNewPrice.priceNew(item.price, item.discountPercentage);
     });
-    const latestProducts = await Product.find(find).sort({position: "desc"}).limit(6);
+    const latestProducts = await Product.find(find).sort({createdAt: -1}).limit(8);
     latestProducts.forEach(item => {
       item.priceNew = calcuNewPrice.priceNew(item.price, item.discountPercentage);
     });
