1c2b8ce1525a1cade270bbc1b57e7e30edaabdfc
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-22 12:52:15 +0700

    feat(middlewares): add function to upload fields

--- middleware/admin/uploadCloud-middleware.js
+++ middleware/admin/uploadCloud-middleware.js
@@ -1,39 +1,41 @@
+const uploadToCloudinary = require("../../helpers/upload-cloudinary");
 
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
-// const storageMulti = require('../../helpers/storeMulti')
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.CLOUD_API_KEY, 
-  api_secret: process.env.CLOUD_API_SECRET
-});
+// Hàm cũ: Dành cho upload 1 file (upload.single)
+module.exports.upload = async (req, res, next) => {
+  try {
+    if (req.file) {
+      const link = await uploadToCloudinary(req.file.buffer);
+      req.body[req.file.fieldname] = link;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports.upload =async (req, res, next)=>{
-      if(req.file){
-        let streamUpload = (req) => {
-        return new Promise((resolve, reject) => {
-            let stream = cloudinary.uploader.upload_stream(
-              (error, result) => {
-                if (result) {
-                  resolve(result);
-                } else {
-                  reject(error);
-                }
-              }
-            );
-
-          streamifier.createReadStream(req.file.buffer).pipe(stream);
-        });
-          };
-          async function upload(req) {
-              let result = await streamUpload(req);
-              //console.log(result.secure_url);
-              req.body[req.file.fieldname] = result.secure_url;
+module.exports.uploadFields = async (req, res, next) => {
+  try {
+    if (req.files) {
+      if (typeof req.files === 'object' && !Array.isArray(req.files)) {
+        for (const fieldname in req.files) {
+          const fileArray = req.files[fieldname];
+          if (fileArray && fileArray.length > 0) {
+            const uploadPromises = fileArray.map(file => uploadToCloudinary(file.buffer));
+            const links = await Promise.all(uploadPromises);
+            req.body[fieldname] = links; 
           }
-          await upload(req);
-          next();
-      }
-      else{
-        next();
+        }
+      } 
+      else if (Array.isArray(req.files) && req.files.length > 0) {
+        const uploadPromises = req.files.map(file => uploadToCloudinary(file.buffer));
+        const links = await Promise.all(uploadPromises);
+        
+        const fieldname = req.files[0].fieldname;
+        req.body[fieldname] = links;
       }
     }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
